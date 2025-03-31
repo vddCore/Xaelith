bc72de6bf3b5ac2af236a9ee416ca69193fe0d5a
vddCore <vddcore@outlook.com>
2025-03-31 13:26:54 +0200

    how about you add all project files you absolute dingus.

--- Xaelith.DataModel/Configuration/Settings.cs
+++ Xaelith.DataModel/Configuration/Settings.cs
@@ -1,6 +1,44 @@
 ﻿namespace Xaelith.DataModel.Configuration;
 
-public class Settings
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+public sealed class Settings : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
     
+    [JsonPropertyName("core")]
+    public CoreSection Core { get; set; } = new();
+
+    [JsonPropertyName("general")]
+    public GeneralSection General { get; set; } = new();
+
+    public Settings()
+    {
+        Core.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(sender, args);
+        General.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(sender, args);
+    }
+
+    public class CoreSection : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        [JsonPropertyName("plugins_directory")]
+        public string PluginsDirectory { get; set; } = "xa-plugins";
+        
+        [JsonPropertyName("data_directory")]
+        public string DataDirectory { get; set; } = "xa-data";
+
+        internal CoreSection()
+        {
+        }
+    }
+
+    public class GeneralSection : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = "Xaelith Blog";
+    }
 }
--- Xaelith.ServiceModel/IConfigurationService.cs
+++ Xaelith.ServiceModel/IConfigurationService.cs
@@ -1,6 +1,12 @@
 ﻿namespace Xaelith.ServiceModel;
 
+using Xaelith.DataModel.Configuration;
+
 public interface IConfigurationService
 {
+    string SettingsFilePath { get; }
+    Settings Settings { get; }
     
+    Task LoadAsync();
+    Task SaveAsync();
 }
